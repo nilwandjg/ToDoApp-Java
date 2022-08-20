21a6fc107f42d9df8ddcb7ae599f8e0882d65014
nilwandjg <nilwandjg@yahoo.com.br>
2022-08-20 03:52:05 -0300

    Segunda correção do primeiro commit.

--- src/main/java/util/ConnectionFactory.java
+++ src/main/java/util/ConnectionFactory.java
@@ -17,7 +17,7 @@ public class ConnectionFactory {
     public static final String DRIVER = "com.mysql.jdbc.Driver";
     public static final String URL = "jdbc:mysql://localhost:3306/todoapp";
     public static final String USER = "root";
-    public static final String PASS = "";
+    public static final String PASS = "@senhaMySQL";
 
     public static Connection getConnection() {
         try {
